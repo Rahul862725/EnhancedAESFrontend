1ba1eabc0374db1bbf632e87dec968d5d975ec3f
tnluser <rahul.kumar245@byjus.com>
2023-04-16 10:27:53 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+import Singup from './components/Signup';
+import Footer from './components/Footer';
+import Decrypted from './components/Decrypted';
+import Encrypted from './components/Encrypted';
+import { Route, Routes } from 'react-router-dom'
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <  > 
+      <Navbar />
+      <Routes>
+      <Route exact path="/" element={<Home/>} />
+        <Route exact path="/login" element={<Login color='black' backgroundColor="white"/>} />
+        <Route exact path="/signup" element={<Singup color='black' backgroundColor="white"/>} />
+        <Route exact path="/decrypt" element={<Decrypted/>} />
+        <Route exact path="/encrypt" element={<Encrypted/>} />
+       
+
+      </Routes>
+      <Footer email="#" github="#" linkedin="#"/>
+    </>
   );
 }
 
