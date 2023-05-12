b1dc383d5a85ff064bbcaf4434c15ebac44f026f
Rahul <rk9248279@gmail.com>
2023-05-12 15:46:29 +0530

    Changes Fotter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import MailIcon from '@mui/icons-material/Mail';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import './style.css'
 
 export default function Footer(props) {
   return (
     <div className='footer'> 
-    <a className='linka' href={`mailto:${props.email}`}><MailIcon/> GMail</a>
-    <a className='linka' href={`${props.github}`}><GitHubIcon/> Github</a>
-    <a className='linka' href={`${props.linkedin}`}><LinkedInIcon/>Linkedin</a>
+    <a className='linka' href={`mailto:${props.email}`}><i class="fa fa-envelope" aria-hidden="true"></i> GMail</a>
+    <a className='linka' href={`${props.github}`}> <i class="fa fa-github" aria-hidden="true"></i>Github</a>
+    <a className='linka' href={`${props.linkedin}`}><i class="fa fa-linkedin" style={{color:"blue",backgroundColor:'white', paddingLeft:'4px', paddingRight:'4px', borderRadius:'4px'}}></i>Linkedin</a>
     </div>
     
   )
